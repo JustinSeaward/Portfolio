bbdea25c5347ac732b7db4d727262d4608c80b2d
Justin Seaward <jut_seaward@yahoo.com>
2025-12-18 23:01:23 -0330

    feature: styling in langing.css and short paragragp and button in landing.jsx

---
+++ student-protfolio/src/components/pages/landing-page/Landing.jsx
@@ -0,0 +1,22 @@
+import "./Landing.css";
+
+const Landing = () => {
+  2;
+  return (
+    <section className="me-section" id="home">
+      <h1>
+        Hi, I'm <span className="highlight">Justin Seaward</span>
+      </h1>
+      <h3>Full Stack Developer in Training</h3>
+      <p>
+        I am a student building web applications and analyzing data. I bridge
+        the gap between Python scripts and dynamic React interfaces.
+      </p>
+      <a href="#projects" className="btn">
+        View My Work
+      </a>
+    </section>
+  );
+};
+
+export default Landing;

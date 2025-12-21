2e68a6d3a1fc2c6a59067915bc56ec789e6c4aa1
Justin Seaward <jut_seaward@yahoo.com>
2025-12-21 15:06:16 -0330

    project: updated project with more information

--- student-protfolio/src/components/project/Project.jsx
+++ student-protfolio/src/components/project/Project.jsx
@@ -3,19 +3,40 @@ import "./Project.css";
 const Projects = () => {
   const projectList = [
     {
+      title: "Steem-App",
+      description:
+        "E-Commerce Web Application where you can browse and add games to a shopping cart. There is a Log-in page with three different users and each with there own wallet amounts. Check it out here ---> https://s2-final.netlify.app/",
+      tech: ["React", "Vite", "CSS"],
+      link: "https://github.com/JustinSeaward/S2_Sprint_Final",
+    },
+    {
+      title: "Portfolio Site",
+      description:
+        "This website! Built to showcase my journey as a student then a working developer. This site will grow with me as long as im developing and leaning new way to code new ideas.",
+      tech: ["React", "Vite", "CSS"],
+      link: "https://github.com/JustinSeaward/Portfolio",
+    },
+    {
+      title: "The One Stop Insurance Company.",
+      description:
+        "The One Stope Insurance company needs a program to enter and calculate new insurance policy information for its customers.",
+      tech: ["Python"],
+      link: "https://github.com/JustinSeaward/QAP4-Python",
+    },
+    {
+      title: "Mo's Lawn Care Service.",
+      description:
+        "Mo’s Lawncare Services is a company that will perform maintenance on your lawn by appointment. Allow the user to enter the Customer name, street address, city, phone number (999-999-9999), and the total number of square feet forthe property (#####).",
+      tech: ["Javascript, HTML, CSS"],
+      link: "https://github.com/JustinSeaward/QAP-3-Files-JS",
+    },
+    {
       title: "Hab Taxi Service",
       description:
         "A Python script that processes data files. Stores and retrieves information for employees, car rentals, payments, revenue, expenses and finances.",
       tech: ["Python"],
       link: "https://github.com/JustinSeaward/HAB_TAXI_SERVICE",
     },
-    {
-      title: "Portfolio Site",
-      description: "This website! Built to showcase my journey as a developer.",
-      tech: ["React", "Vite", "CSS"],
-      link: "",
-    },
-    // Add more projects here later!
   ];
 
   return (
@@ -33,7 +54,7 @@ const Projects = () => {
                 </span>
               ))}
             </div>
-            <a href={project.link} className="project-link">
+            <a href={project.link} target="_blank" className="project-link">
               View Code &rarr;
             </a>
           </div>
